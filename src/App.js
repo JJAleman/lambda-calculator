c88c9d6d3d6265f2ec62aca0ccff2c5ffea44689
Juan Aleman <juan.j.aleman@gmail.com>
2019-09-25 17:04:30 -0500

    Numbers and Operaters display now

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import Numbers from "./components/ButtonComponents/NumberButtons/Numbers.js";
+import Operators from "./components/ButtonComponents/OperatorButtons/Operators.js";
 // STEP 4 - import the button and display components
 // Don't forget to import any extra css/scss files you build into the correct component
 
@@ -7,6 +9,10 @@ import "./App.css";
 import Logo from "./components/DisplayComponents/Logo";
 
 function App() {
+
+  const [displayNums, setDisplayNums] = useState("");
+  const [displayOps, setDisplayOps] = useState("");
+
   // STEP 5 - After you get the components displaying using the provided data file, write your state hooks here.
   // Once the state hooks are in place write some functions to hold data in state and update that data depending on what it needs to be doing
   // Your functions should accept a parameter of the the item data being displayed to the DOM (ie - should recieve 5 if the user clicks on
@@ -17,6 +23,10 @@ function App() {
     <div className="container">
       <Logo />
       <div className="App">
+        <Numbers display={displayNums} setDisplay={setDisplayNums} />
+        <Operators display={displayOps} setDisplay={setDisplayOps}/>
+
+        {/* <Operators display={displayOps} setDisplayOps={setDisplayOps} /> */}
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
       </div>
     </div>
--- src/components/ButtonComponents/OperatorButtons/OperatorButton.js
+++ src/components/ButtonComponents/OperatorButtons/OperatorButton.js
@@ -1,9 +1,14 @@
 import React from "react";
 
-const OperatorButton = () => {
+const OperatorButton = props => {
+  console.log("Props for OperatorButton", props)
   return (
-    <>
+    <button>
+
+      {props.operators.char}
       {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
+    </button>
   );
 };
+
+export default OperatorButton;
